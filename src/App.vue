07ba14da7abf7a176af0a65ca774223fce0ae3cf
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-08 13:59:08 +0200

    feat: store.js + dependencies + tests for working code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script>
+import {store} from './store.js';
+
 export default {
-  
+    data() {
+        return {
+            store,
+        }
+    },
 }
 </script>
 
 <template>
-  
+    <h1>{{this.store.apiKey}}</h1>
 </template>
 
 <style lang="scss">
-  
+
 </style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import {reactive} from "vue";
+
+export const store = reactive({
+    apiKey: "8d15f1ddc14dc7f7817d1892051908af",
+    searchQuery: "",
+    moviesArray: [],
+    tvArray: [],
+});
