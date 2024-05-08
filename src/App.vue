1ca160928a20a7abf942db1dce522ecc498c3f39
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-08 15:05:15 +0200

    feat: API call on click + debugging for query value in store.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <script>
 import {store} from './store.js';
+import AppHeader from './components/AppHeader.vue';
+import axios from "axios";
 
 export default {
+    components: { 
+        AppHeader 
+    },
     data() {
         return {
             store,
+        }
+    },
+    methods: {
+        getAPI() {
+            axios.get("https://api.themoviedb.org/3/search/movie", {params: {api_key: this.store.apiKey, query: this.store.searchQuery}}).then((resp) => {
+                console.log(resp.data.results);
+            })
         }
     },
 }
 </script>
 
 <template>
-    <h1>{{this.store.apiKey}}</h1>
+    <div>
+        <AppHeader @filter="getAPI"/>
+    </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+import {store} from "../store";
+export default {
+    data() {
+        return {
+            query: "",
+        }
+    },
+    methods: {
+        emitSaveQuery() {
+            console.log(this.query);
+            store.searchQuery = this.query;
+            console.log(this.query);
+            this.$emit('filter', this.query);
+            console.log(this.query);
+        }
+    },
+}
+</script>
+
+<template>
+    <!-- container -->
+    <div>
+        <!-- searchbar -->
+        <input type="text" v-model="query" placeholder="Cerca un film...">
+        <!-- search-button -->
+        <button @click="emitSaveQuery">Cerca</button>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
