f02b03c7d5ea8b3f7664f4c92ef4772f054a115c
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-09 19:17:25 +0200

    feat: header style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,12 +17,22 @@ export default {
 
 <template>
     <!-- container -->
-    <div>
+    <header class="bg-black d-flex py-4 justify-content-between align-items-center">
+        <div class="ms-5">
+            <a href="#" class="text-decoration-none">
+                <h1 class="text-danger fw-semibold">BOOLFLIX</h1>
+            </a>
+        </div>
         <!-- searchbar -->
-        <input type="text" v-model="query" placeholder="Cerca un film...">
-        <!-- search-button -->
-        <button @click="emitSaveQuery">Cerca</button>
-    </div>
+        <div class="me-5">
+            <input type="text" v-model="query" placeholder="Cerca un film...">
+            <!-- search-button -->
+            <button @click="emitSaveQuery">Cerca</button>
+        </div>
+
+    </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
